02021f7e3498230832875b0a3d5f1c0193437eca
tosi4ka <antonshestakov124@gmail.com>
2026-04-30 13:28:19 +0200

    feat(tasks): add ListTasks handler and route

--- backend/internal/server/roter.go
+++ backend/internal/server/roter.go
@@ -16,5 +16,6 @@ func SetupRouter(r *gin.Engine, auth *AuthHandler, jwtSecret string, task *task.
 	{
 		protected.POST("/task/createTask", task.CreateTask)
 		protected.PATCH("/task/updateTask", task.UpdateTask)
+		protected.GET("/task/tasksList/:id", task.ListTasks)
 	}
 }
